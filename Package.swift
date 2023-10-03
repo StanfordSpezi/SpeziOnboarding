86e2b3c398eeca2f23684b75cd8db5279e39011f
Philipp Zagar <philipp.zagar@gmail.com>
2023-10-03 18:05:02 +0200

    dependency fix

--- Package.swift
+++ Package.swift
@@ -21,13 +21,14 @@ let package = Package(
         .library(name: "SpeziOnboarding", targets: ["SpeziOnboarding"])
     ],
     dependencies: [
-        .package(url: "https://github.com/StanfordSpezi/Spezi", .upToNextMinor(from: "0.7.2")),
+        .package(url: "https://github.com/StanfordSpezi/Spezi", .upToNextMinor(from: "0.7.0")),
         .package(url: "https://github.com/StanfordSpezi/SpeziViews", .upToNextMinor(from: "0.5.0"))
     ],
     targets: [
         .target(
             name: "SpeziOnboarding",
             dependencies: [
+                .product(name: "Spezi", package: "Spezi"),
                 .product(name: "SpeziViews", package: "SpeziViews")
             ]
         ),
