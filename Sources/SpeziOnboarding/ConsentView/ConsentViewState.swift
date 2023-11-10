3487479f6f08c9ff598b5b42dad4a1be71205ccc
Andreas Bauer <andreas.bauer@stanford.edu>
2023-11-10 13:06:41 -0800

    Fix documentation

--- Sources/SpeziOnboarding/ConsentView/ConsentViewState.swift
+++ Sources/SpeziOnboarding/ConsentView/ConsentViewState.swift
@@ -15,21 +15,21 @@ import SwiftUI
 /// It can be used to observe and control the behaviour of the ``ConsentDocument``, especially in regards
 /// to the export functionality.
 public enum ConsentViewState: Equatable {
-    /// The ``ConsentDocument/ConsentViewState/base(_:)`` state utilizes the
+    /// The ``ConsentViewState/base(_:)`` state utilizes the
     /// SpeziViews `ViewState`'s to indicate the state of the ``ConsentDocument``, either `.idle`, `.processing`, or `.error(_:)`.
     case base(SpeziViews.ViewState)
-    /// The ``ConsentDocument/ConsentViewState/namesEntered`` state signifies that all required name fields in the ``ConsentDocument`` view have been completed.
+    /// The ``ConsentViewState/namesEntered`` state signifies that all required name fields in the ``ConsentDocument`` view have been completed.
     case namesEntered
-    /// The ``ConsentDocument/ConsentViewState/signing`` state indicates that the ``ConsentDocument`` is currently being signed by the user.
+    /// The ``ConsentViewState/signing`` state indicates that the ``ConsentDocument`` is currently being signed by the user.
     case signing
-    /// The ``ConsentDocument/ConsentViewState/signed`` state indicates that the ``ConsentDocument`` is signed by the user.
+    /// The ``ConsentViewState/signed`` state indicates that the ``ConsentDocument`` is signed by the user.
     case signed
-    /// The ``ConsentDocument/ConsentViewState/export`` state can be set by an outside view
+    /// The ``ConsentViewState/export`` state can be set by an outside view
     /// encapsulating the ``ConsentDocument`` to trigger the export of the consent document as a PDF.
-    /// The previous state must be ``ConsentDocument/ConsentViewState/signed``, indicating that the consent document is signed.
+    /// The previous state must be ``ConsentViewState/signed``, indicating that the consent document is signed.
     case export
-    /// The ``ConsentDocument/ConsentViewState/exported(document:)`` state indicates that the
+    /// The ``ConsentViewState/exported(document:)`` state indicates that the
     /// ``ConsentDocument`` has been successfully exported. The rendered `PDFDocument` can be found as the associated value of the state.
-    /// The export procedure (resulting in the ``ConsentDocument/ConsentViewState/exported(document:)`` state) can be triggered via setting the ``ConsentDocument/ConsentViewState/export`` state of the ``ConsentDocument``    .
+    /// The export procedure (resulting in the ``ConsentViewState/exported(document:)`` state) can be triggered via setting the ``ConsentViewState/export`` state of the ``ConsentDocument``    .
     case exported(document: PDFDocument)
 }
