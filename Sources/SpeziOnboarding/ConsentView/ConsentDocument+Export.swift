457b273f759d9812e83185575828d5dfe001081c
Patrick Langer <patrick.langer97@gmx.de>
2024-06-26 12:20:16 +0200

    Resolved swiftlint issues. Changed rendering to use ImageRenderer instead of UIGraphicsImageRenderer for compatibility with macOS and visionOS.

--- Sources/SpeziOnboarding/ConsentView/ConsentDocument+Export.swift
+++ Sources/SpeziOnboarding/ConsentView/ConsentDocument+Export.swift
@@ -9,7 +9,7 @@
 import PDFKit
 import PencilKit
 import SwiftUI
-import WebKit
+
 /// Extension of `ConsentDocument` enabling the export of the signed consent page.
 extension ConsentDocument {
 #if !os(macOS)
@@ -75,50 +75,42 @@ extension ConsentDocument {
 
         let pages = paginatedViews(markdown: markdownString)
 
-        print("NumPages: \(pages.count)")
-    
+        let paperSize = CGSize(
+            width: exportConfiguration.paperSize.dimensions.width,
+            height: exportConfiguration.paperSize.dimensions.height
+        )
+
         guard let mutableData = CFDataCreateMutable(kCFAllocatorDefault, 0),
                 let consumer = CGDataConsumer(data: mutableData),
                 let pdf = CGContext(consumer: consumer, mediaBox: nil, nil) else {
-            
-            return nil;
+            return nil
         }
 
         for page in pages {
-            pdf.beginPDFPage(nil)
+            let renderer = ImageRenderer(content: page)
             
-            let hostingController = UIHostingController(rootView: page)
-                hostingController.view.frame = CGRect(origin: .zero, size: pageSize)
-
-                let renderer = UIGraphicsImageRenderer(bounds: hostingController.view.bounds)
-                let image = renderer.image { ctx in
-                    hostingController.view.drawHierarchy(in: hostingController.view.bounds, afterScreenUpdates: true)
-                }
-
-            pdf.saveGState()
-
-            pdf.translateBy(x: 0, y: pageSize.height)
-            pdf.scaleBy(x: 1.0, y: -1.0)
-
-            hostingController.view.layer.render(in: pdf)
-
-            pdf.restoreGState()
+            renderer.render { _, context in
+                var box = CGRect(origin: .zero, size: paperSize)
+       
+                pdf.beginPDFPage(nil)
+                pdf.translateBy(x: 0, y: 0)
                 
-            
-            pdf.endPDFPage()
+                context(pdf)
+                
+                pdf.endPDFPage()
+            }
         }
 
         pdf.closePDF()
-        return PDFDocument(data: mutableData as Data); 
+        return PDFDocument(data: mutableData as Data)
     }
     
-    private func paginatedViews(markdown: AttributedString) -> [AnyView] 
-    {
+    private func paginatedViews(markdown: AttributedString) -> [AnyView] {
         /*
         This algorithm splits the consent document consisting of title, text and signature
         across multiple pages, if titleHeight + signatureHeight + textHeight is larger than 1 page. 
         
-        Let's call header = title, and footer = signature.
+        Let header = title, and footer = signature.
         
         The algorithm ensures that headerHeight + footerHeight + textHeight <= pageHeight.
         headerHeight is set to 200 on the first page, and to 50 on all subsequent pages (as we do not have a title anymore).
@@ -146,41 +138,26 @@ extension ConsentDocument {
         // Header height on all subsequent pages. Should come from exportConfiguration.
         let headerHeightOtherPages: CGFloat = 50
         let footerHeight: CGFloat = 150
-
         var headerHeight = headerHeightFirstPage
 
         while !remainingMarkdown.unicodeScalars.isEmpty {
-            let (currentPageContent, nextPageContent) = split(markdown: remainingMarkdown, pageSize: pageSize, headerHeight: headerHeight, footerHeight: footerHeight)
+            let (currentPageContent, nextPageContent) =
+                split(markdown: remainingMarkdown, pageSize: pageSize, headerHeight: headerHeight, footerHeight: footerHeight)
             
             // In the first iteration, headerHeight was headerHeightFirstPage.
             // In all subsequent iterations, we only need headerHeightOtherPages.
             // Hence, more text fits on the page.
-            headerHeight = headerHeightOtherPages;
+            headerHeight = headerHeightOtherPages
 
-            let currentPage: AnyView = AnyView(
+            let currentPage = AnyView(
                 VStack {
                     if pages.isEmpty {  // First page
-                        
-                        VStack{
-                            if exportConfiguration.includingTimestamp {
-                                HStack {
-                                    Spacer()
-
-                                    Text("EXPORTED_TAG", bundle: .module)
-                                        + Text(verbatim: ": \(DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short))")
-                                }
-                                .font(.caption)
-                                .padding()
-                            }
-                            
-                            OnboardingTitleView(title: exportConfiguration.consentTitle)
-                        }
-                        .padding()
-
+                        renderTitle(headerHeight: headerHeight)
                     } else {
                         // If we are not on the first page, we add a spacing of headerHeight,
                         // which should now be set to "headerHeightOtherPages".
-                        VStack{}.padding(.top, headerHeight)
+                        VStack {
+                        }.padding(.top, headerHeight)
                     }
 
                     Text(currentPageContent)
@@ -189,33 +166,23 @@ extension ConsentDocument {
                     Spacer()
 
                     if nextPageContent.unicodeScalars.isEmpty {  // Last page
-                        ZStack(alignment: .bottomLeading) {
-                            SignatureViewBackground(name: name, backgroundColor: .clear)
-
-                            #if !os(macOS)
-                            Image(uiImage: blackInkSignatureImage)
-                            #else
-                            Text(signature)
-                                .padding(.bottom, 32)
-                                .padding(.leading, 46)
-                                .font(.custom("Snell Roundhand", size: 24))
-                            #endif
-                        }
-                        .padding(.bottom, footerHeight)
+                        renderSignature(footerHeight: footerHeight)
                     }
                 }
                 .frame(width: pageSize.width, height: pageSize.height)
             )
-
             pages.append(currentPage)
             remainingMarkdown = nextPageContent
         }
-
         return pages
     }
 
-    private func split(markdown: AttributedString, pageSize: CGSize, headerHeight: CGFloat, footerHeight: CGFloat) -> (AttributedString, AttributedString) 
-    {
+    private func split(
+        markdown: AttributedString,
+        pageSize: CGSize,
+        headerHeight: CGFloat,
+        footerHeight: CGFloat
+    ) -> (AttributedString, AttributedString) {
         // This algorithm determines at which index to split the text, if textHeight + headerHeight + footerHeight > pageSize.
         // The algorithm returns the text that still fits on the current page,
         // and the remaining text which needs to be placed on subsequent page(s).
@@ -242,12 +209,53 @@ extension ConsentDocument {
         layoutManager.addTextContainer(textContainer)
         textStorage.addLayoutManager(layoutManager)
 
-        var accumulatedHeight: CGFloat = 0       
+        var accumulatedHeight: CGFloat = 0
         let maximumRange = layoutManager.glyphRange(for: textContainer)
      
         currentPage = AttributedString(textStorage.attributedSubstring(from: maximumRange))
-        remaining = AttributedString(textStorage.attributedSubstring(from: NSRange(location: maximumRange.length, length: textStorage.length - maximumRange.length)))
+        remaining = AttributedString(
+                textStorage.attributedSubstring(
+                        from: NSRange(
+                            location: maximumRange.length,
+                            length: textStorage.length - maximumRange.length
+                        )
+                    )
+                )
 
         return (currentPage, remaining)
+    }
+    
+    // Creates a View for the title, which can then be rendered during PDF export.
+    private func renderTitle(headerHeight: CGFloat) -> some View {
+        VStack {
+            if exportConfiguration.includingTimestamp {
+                HStack {
+                    Spacer()
+                    Text("EXPORTED_TAG", bundle: .module) + Text(verbatim: ": "
+                        + DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short))
+                }
+                .font(.caption)
+                .padding()
+            }
+            OnboardingTitleView(title: exportConfiguration.consentTitle)
+        }
+        .padding()
+    }
+
+    // Creates a View for the signature, which can then be rendered during PDF export.
+    private func renderSignature(footerHeight: CGFloat) -> some View {
+        ZStack(alignment: .bottomLeading) {
+            SignatureViewBackground(name: name, backgroundColor: .clear)
+            
+            #if !os(macOS)
+            Image(uiImage: blackInkSignatureImage)
+            #else
+            Text(signature)
+                .padding(.bottom, 32)
+                .padding(.leading, 46)
+                .font(.custom("Snell Roundhand", size: 24))
+            #endif
+        }
+        .padding(.bottom, footerHeight)
     }
 }
