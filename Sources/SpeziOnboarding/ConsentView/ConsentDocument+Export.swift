b3c0ffac2373575e009971b2d984d53f80cccb27
Andreas Bauer <andreas.bauer@stanford.edu>
2024-08-11 11:14:30 +0200

    Resolve some compiler errors

--- Sources/SpeziOnboarding/ConsentView/ConsentDocument+Export.swift
+++ Sources/SpeziOnboarding/ConsentView/ConsentDocument+Export.swift
@@ -16,7 +16,7 @@ extension ConsentDocument {
     #if !os(macOS)
     /// As the `PKDrawing.image()` function automatically converts the ink color dependent on the used color scheme (light or dark mode),
     /// force the ink used in the `UIImage` of the `PKDrawing` to always be black by adjusting the signature ink according to the color scheme.
-    private var blackInkSignatureImage: UIImage {
+    @MainActor private var blackInkSignatureImage: UIImage {
         var updatedDrawing = PKDrawing()
         
         for stroke in signature.strokes {
